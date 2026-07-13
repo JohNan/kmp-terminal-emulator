a06d4a4e1ef1d4e05ddd91bb6c9c2eb1dd276445
Johan Nenzén <johan.nanzen@gmail.com>
2026-07-13 23:03:07 +0000

    Add manual publish-npm GitHub Actions workflow and build-npm packaging script

---
+++ build-npm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "Building production WebAssembly bundle..."
+mise exec -- ./gradlew :demo-web:wasmJsBrowserDistribution --no-daemon
+
+echo "Preparing npm package directory..."
+rm -rf npm-package
+mkdir -p npm-package
+
+# Copy build outputs
+cp -r demo-web/build/dist/wasmJs/productionExecutable/* npm-package/
+
+# Create package.json inside the npm package
+cat <<EOF > npm-package/package.json
+{
+  "name": "@JohNan/kmp-terminal",
+  "version": "0.1.0",
+  "description": "Kotlin Multiplatform Terminal Emulator compiled to WebAssembly",
+  "main": "demo-web.js",
+  "files": [
+    "demo-web.js",
+    "*.wasm",
+    "composeResources/**/*"
+  ],
+  "publishConfig": {
+    "registry": "https://npm.pkg.github.com"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git@github.com:JohNan/kmp-terminal-emulator.git"
+  }
+}
+EOF
+
+echo "NPM Package prepared under npm-package/!"
