f5e142dcdba584cad0a098f2c32eedf33a34409b
JohNan <johan@antigravity.dev>
2026-08-16 21:47:31 +0000

    style: format KeyHandler with ktlint

--- terminal-ui/src/androidMain/kotlin/com/sshclient/composeapp/presentation/screens/terminal/KeyHandler.kt
+++ terminal-ui/src/androidMain/kotlin/com/sshclient/composeapp/presentation/screens/terminal/KeyHandler.kt
@@ -54,12 +54,30 @@ fun handleAndroidHardwareKeyEvent(
         }
 
         when (keyCode) {
-            KeyEvent.KEYCODE_LEFT_BRACKET -> { onInput.invoke("\u001B"); return true } // ESC
-            KeyEvent.KEYCODE_BACKSLASH -> { onInput.invoke("\u001C"); return true } // FS
-            KeyEvent.KEYCODE_RIGHT_BRACKET -> { onInput.invoke("\u001D"); return true } // GS
-            KeyEvent.KEYCODE_6 -> { onInput.invoke("\u001E"); return true } // RS (Ctrl+6)
-            KeyEvent.KEYCODE_MINUS -> { onInput.invoke("\u001F"); return true } // US (Ctrl+-)
-            KeyEvent.KEYCODE_DEL -> { onInput.invoke("\u0017"); return true } // Ctrl+Backspace -> Ctrl+W
+            KeyEvent.KEYCODE_LEFT_BRACKET -> {
+                onInput.invoke("\u001B")
+                return true
+            } // ESC
+            KeyEvent.KEYCODE_BACKSLASH -> {
+                onInput.invoke("\u001C")
+                return true
+            } // FS
+            KeyEvent.KEYCODE_RIGHT_BRACKET -> {
+                onInput.invoke("\u001D")
+                return true
+            } // GS
+            KeyEvent.KEYCODE_6 -> {
+                onInput.invoke("\u001E")
+                return true
+            } // RS (Ctrl+6)
+            KeyEvent.KEYCODE_MINUS -> {
+                onInput.invoke("\u001F")
+                return true
+            } // US (Ctrl+-)
+            KeyEvent.KEYCODE_DEL -> {
+                onInput.invoke("\u0017")
+                return true
+            } // Ctrl+Backspace -> Ctrl+W
         }
     }
 
@@ -72,39 +90,111 @@ fun handleAndroidHardwareKeyEvent(
             return true
         }
         when (keyCode) {
-            KeyEvent.KEYCODE_PERIOD -> { onInput.invoke("\u001B."); return true }
-            KeyEvent.KEYCODE_DEL -> { onInput.invoke("\u001B\u007F"); return true } // Alt+Backspace
+            KeyEvent.KEYCODE_PERIOD -> {
+                onInput.invoke("\u001B.")
+                return true
+            }
+            KeyEvent.KEYCODE_DEL -> {
+                onInput.invoke("\u001B\u007F")
+                return true
+            } // Alt+Backspace
         }
     }
 
     // Function keys F1-F12
     when (keyCode) {
-        KeyEvent.KEYCODE_F1 -> { onInput.invoke("\u001BOP"); return true }
-        KeyEvent.KEYCODE_F2 -> { onInput.invoke("\u001BOQ"); return true }
-        KeyEvent.KEYCODE_F3 -> { onInput.invoke("\u001BOR"); return true }
-        KeyEvent.KEYCODE_F4 -> { onInput.invoke("\u001BOS"); return true }
-        KeyEvent.KEYCODE_F5 -> { onInput.invoke("\u001B[15~"); return true }
-        KeyEvent.KEYCODE_F6 -> { onInput.invoke("\u001B[17~"); return true }
-        KeyEvent.KEYCODE_F7 -> { onInput.invoke("\u001B[18~"); return true }
-        KeyEvent.KEYCODE_F8 -> { onInput.invoke("\u001B[19~"); return true }
-        KeyEvent.KEYCODE_F9 -> { onInput.invoke("\u001B[20~"); return true }
-        KeyEvent.KEYCODE_F10 -> { onInput.invoke("\u001B[21~"); return true }
-        KeyEvent.KEYCODE_F11 -> { onInput.invoke("\u001B[23~"); return true }
-        KeyEvent.KEYCODE_F12 -> { onInput.invoke("\u001B[24~"); return true }
+        KeyEvent.KEYCODE_F1 -> {
+            onInput.invoke("\u001BOP")
+            return true
+        }
+        KeyEvent.KEYCODE_F2 -> {
+            onInput.invoke("\u001BOQ")
+            return true
+        }
+        KeyEvent.KEYCODE_F3 -> {
+            onInput.invoke("\u001BOR")
+            return true
+        }
+        KeyEvent.KEYCODE_F4 -> {
+            onInput.invoke("\u001BOS")
+            return true
+        }
+        KeyEvent.KEYCODE_F5 -> {
+            onInput.invoke("\u001B[15~")
+            return true
+        }
+        KeyEvent.KEYCODE_F6 -> {
+            onInput.invoke("\u001B[17~")
+            return true
+        }
+        KeyEvent.KEYCODE_F7 -> {
+            onInput.invoke("\u001B[18~")
+            return true
+        }
+        KeyEvent.KEYCODE_F8 -> {
+            onInput.invoke("\u001B[19~")
+            return true
+        }
+        KeyEvent.KEYCODE_F9 -> {
+            onInput.invoke("\u001B[20~")
+            return true
+        }
+        KeyEvent.KEYCODE_F10 -> {
+            onInput.invoke("\u001B[21~")
+            return true
+        }
+        KeyEvent.KEYCODE_F11 -> {
+            onInput.invoke("\u001B[23~")
+            return true
+        }
+        KeyEvent.KEYCODE_F12 -> {
+            onInput.invoke("\u001B[24~")
+            return true
+        }
     }
 
     // Handle special keys without modifiers (or ignoring modifiers where appropriate)
     when (keyCode) {
-        KeyEvent.KEYCODE_ENTER -> { onInput.invoke("\r"); return true } // \r is CARRIAGE_RETURN
-        KeyEvent.KEYCODE_DEL -> { onInput.invoke("\u007F"); return true } // \u007F is DELETE_CHAR
-        KeyEvent.KEYCODE_TAB -> { onInput.invoke("\t"); return true } // \t is TAB_CHAR
-        KeyEvent.KEYCODE_ESCAPE -> { onInput.invoke("\u001B"); return true } // \u001B is ESCAPE_CHAR
-        KeyEvent.KEYCODE_FORWARD_DEL -> { onInput.invoke("\u001B[3~"); return true } // Delete key
-        KeyEvent.KEYCODE_INSERT -> { onInput.invoke("\u001B[2~"); return true }
-        KeyEvent.KEYCODE_PAGE_UP -> { onInput.invoke("\u001B[5~"); return true }
-        KeyEvent.KEYCODE_PAGE_DOWN -> { onInput.invoke("\u001B[6~"); return true }
-        KeyEvent.KEYCODE_MOVE_HOME -> { onInput.invoke("\u001B[H"); return true }
-        KeyEvent.KEYCODE_MOVE_END -> { onInput.invoke("\u001B[F"); return true }
+        KeyEvent.KEYCODE_ENTER -> {
+            onInput.invoke("\r")
+            return true
+        } // \r is CARRIAGE_RETURN
+        KeyEvent.KEYCODE_DEL -> {
+            onInput.invoke("\u007F")
+            return true
+        } // \u007F is DELETE_CHAR
+        KeyEvent.KEYCODE_TAB -> {
+            onInput.invoke("\t")
+            return true
+        } // \t is TAB_CHAR
+        KeyEvent.KEYCODE_ESCAPE -> {
+            onInput.invoke("\u001B")
+            return true
+        } // \u001B is ESCAPE_CHAR
+        KeyEvent.KEYCODE_FORWARD_DEL -> {
+            onInput.invoke("\u001B[3~")
+            return true
+        } // Delete key
+        KeyEvent.KEYCODE_INSERT -> {
+            onInput.invoke("\u001B[2~")
+            return true
+        }
+        KeyEvent.KEYCODE_PAGE_UP -> {
+            onInput.invoke("\u001B[5~")
+            return true
+        }
+        KeyEvent.KEYCODE_PAGE_DOWN -> {
+            onInput.invoke("\u001B[6~")
+            return true
+        }
+        KeyEvent.KEYCODE_MOVE_HOME -> {
+            onInput.invoke("\u001B[H")
+            return true
+        }
+        KeyEvent.KEYCODE_MOVE_END -> {
+            onInput.invoke("\u001B[F")
+            return true
+        }
     }
 
     return false
